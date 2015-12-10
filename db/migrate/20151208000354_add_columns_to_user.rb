4bd6e2ede8e4718c5994d9863c4f1dfff5a0f225
Glenn Sullivan <sullivangln87@gmail.com>
2015-12-10 15:49:14 -0700

    fix migrations

--- db/migrate/20151208000354_add_columns_to_user.rb
+++ db/migrate/20151208000354_add_columns_to_user.rb
@@ -2,10 +2,10 @@ class AddColumnsToUser < ActiveRecord::Migration
   has_one :profile
   def change
     add_column :users, :name, :string
-    # add_column :users, :gender, :string
-    # add_column :users, :age, :integer
-    # add_column :users, :interested_in_male, :boolean
-    # add_column :users, :interested_in_female, :boolean
+    add_column :users, :gender, :string
+    add_column :users, :age, :integer
+    add_column :users, :interested_in_male, :boolean
+    add_column :users, :interested_in_female, :boolean
 
 
 
